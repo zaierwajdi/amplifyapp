182cffe42d727c9cb03694ebcd0f98f54a0b22c8
zaierwajdi82 <zaier_wajdi@hotmail.com>
2021-06-09 09:59:06 -0400

    added MAJ

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
